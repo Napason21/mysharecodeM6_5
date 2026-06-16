e79f4fe57d9657557e38577ae784d752c0c88a9c
Napason21 <164160@wsk.ac.th>
2026-06-16 04:39:31 +0000

    trist01.py

--- trist01.py
+++ trist01.py
@@ -1,6 +1,7 @@
 print("โปรแกรมคำนวณหาพื้นที่ของสามเหลี่ยม")
-h=float(input("enter hight"))
+print("โปรแกรมคำนวณหาพื้นที่ของสามเหลี่ยม")
+h=float(input("enter hight:"))
 b=float(input("enter base:"))
 area=0.5*h*b
-print("พื้นที่ของสามเหลี่ยม คือ",area)
+print("พื้นที่ของสามเหลี่ยมเท่ากับ",area)
 print("จัดทำโดย นางสาว นภศร ทองอินทร์โชติ เลขที่21")
