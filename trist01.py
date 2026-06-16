6825026c6fe1c865d465c0d8bb561cd69956d616
Napason21 <164160@wsk.ac.th>
2026-06-16 04:19:39 +0000

    trist01.py M

--- trist01.py
+++ trist01.py
@@ -1,1 +1,6 @@
-print("Hello worid")
+print("โปรแกรมคำนวณหาพื้นที่ของสามเหลี่ยม")
+h=float(input("enter hight"))
+b=float(input("enter base:"))
+area=0.5*h*b
+print("พื้นที่ของสามเหลี่ยม คือ",area)
+print("จัดทำโดย นางสาว นภศร ทองอินทร์โชติ เลขที่21")
